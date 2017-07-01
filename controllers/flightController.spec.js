9ee7bad2adc55d13779343cc31889094c0fbc1a2
Bo Chen <bochen2014@yahoo.com>
2017-07-01 15:42:33 +1000

    todo:test invalid input, invalid json, valid json but invalid keys..etc

---
+++ controllers/flightController.spec.js
@@ -0,0 +1,9 @@
+import * as flightController from './flightController';
+
+describe("flight-controller", () => {
+    it("should handle empty array", () => {
+        // todo: mock req, res, next
+        // verify all negative cases are handled properly
+        expect(true).toBe(true);
+    });
+});

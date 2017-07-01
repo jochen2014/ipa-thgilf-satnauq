ef1dd08d0c18eafa4387a3467f20b38b42dcc73b
Bo Chen <bochen2014@yahoo.com>
2017-07-01 18:36:23 +1000

    __$ignoreMe__: finish unitest todos

--- controllers/flightController.spec.js
+++ controllers/flightController.spec.js
@@ -1,9 +1,36 @@
 import * as flightController from './flightController';
 
 describe("flight-controller", () => {
+    let res;
+    beforeEach(() => {
+        res = {
+            json: jest.fn(),
+        };
+    });
+
     it("should handle empty array", () => {
-        // todo: mock req, res, next
-        // verify all negative cases are handled properly
-        expect(true).toBe(true);
+        const req = {
+            body: {
+                flights: [],
+            },
+        };
+        flightController.filterFlight(req, res);
+        // first para in first call
+        expect(res.json.mock.calls[0][0]).toEqual(
+            { flights: [] },
+        );
+    });
+    it("should throw error with invalid JSON", () => {
+        const req = {
+            body: {
+                flights: [{
+                    _aireline: "QF",
+                }],
+            },
+        };
+        // https://github.com/facebook/jest/issues/781#issuecomment-193991055
+        expect(() => {
+            flightController.filterFlight(req, res);
+        }).toThrow(SyntaxError);
     });
 });

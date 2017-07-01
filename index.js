d4bf7cdb7884dcc09ae5e0e4f46d44ec807d4644
Bo Chen <bochen2014@yahoo.com>
2017-07-01 13:41:57 +1000

    before I set up project structure

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 import 'babel-polyfill';
 import express from 'express';
+import bodyParser from 'body-parser';
+
 const app = express();
 const port = process.env.port || 8000;
 
-app.use('/', (req, res) => {
-    res.send('hello,world!');
-})
+app.use(bodyParser.json());
+app.use('/flights', (req, res) => {
+    res.json({
+        greeting: 'hello,world'
+    })
+});
+
 app.listen(port);
 console.log(`server is listening on http://localhost:${port}`);

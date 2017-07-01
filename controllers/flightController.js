e17246d2b9cff95e2a78a424c37ff1a5d4ef61d8
Bo Chen <bochen2014@yahoo.com>
2017-07-01 14:18:21 +1000

    getting real. before I do the actual filter logic

--- controllers/flightController.js
+++ controllers/flightController.js
@@ -1,8 +1,15 @@
 import * as service from './flightService';
 
 const filterFlight = (req, res, next) => {
-    const result = service.filterFlight();
-    res.json(result);
+    const { flights } = req.body;
+    if (!flights || flights.length === 0) {
+        res.json({
+            flights: [],
+        });
+    } else {
+        const result = service.filterFlight();
+        res.json(result);
+    }
 };
 
 export {

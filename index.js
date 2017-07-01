d8d766e7c877f507e364b70c38a88a9301b767c8
Bo Chen <bochen2014@yahoo.com>
2017-07-01 13:39:19 +1000

    express hello,world done

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 import 'babel-polyfill';
 import express from 'express';
+const app = express();
+const port = process.env.port || 8000;
 
-console.log('hello,world');
+app.use('/', (req, res) => {
+    res.send('hello,world!');
+})
+app.listen(port);
+console.log(`server is listening on http://localhost:${port}`);
